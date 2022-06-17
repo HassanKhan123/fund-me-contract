8ef0255e8d1485bb7e64aecd49655b4c6020f5cb
HassanKhan123 <hk2602809@gmail.com>
2022-06-17 20:37:21 +0500

    added: condition for minimum amout to send

---
+++ Fundme.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+contract Fundme {
+    uint256 public minimumUsd = 50;
+
+    function fund() public payable {
+        require(msg.value >= minimumUsd, "Didn;t send enough eth");
+    }
+}
