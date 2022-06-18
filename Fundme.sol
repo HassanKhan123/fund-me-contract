8e393f94d42c7d9326268a816819c27a30905035
HassanKhan123 <hk2602809@gmail.com>
2022-06-18 16:48:40 +0500

    added: functionality to withdraw funds

--- Fundme.sol
+++ Fundme.sol
@@ -8,14 +8,44 @@ contract Fundme {
     uint256 public minimumUsd = 50 * 1e18;
 
     address[] public funders;
-    mapping(address => uint256) public addressToAmountFunded;
+    mapping(address=>uint256) public addressToAmountFunded;
+
+    address public owner;
+    
+    constructor(){
+        owner = msg.sender;
+    }
 
     function fund() public payable {
-        require(
-            msg.value.getConversionRate() >= minimumUsd,
-            "Didn;t send enough eth"
-        );
+        require(msg.value.getConversionRate() >= minimumUsd,"Didn;t send enough eth");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = msg.value;
     }
+
+    function withdraw() public onlyOwner {
+        for(uint256 funderIndex=0;funderIndex < funders.length;funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+
+        funders = new address[](0);
+
+        // transfer
+        // payable(msg.sender).transfer(address(this).balance);
+
+        //send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess,"Send Failed");
+
+        //call
+        (bool callSuccess, ) = payable(msg.sender).call{value:address(this).balance}("");
+         require(callSuccess,"Call Failed");
+    }
+
+    modifier onlyOwner {
+         require(msg.sender == owner,"Sender is not owner!");
+         _;
+    }
+
+    
 }
