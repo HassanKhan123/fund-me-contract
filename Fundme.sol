ea08d710494c9388133e9b4427491da9cc51aeb2
HassanKhan123 <hk2602809@gmail.com>
2022-06-18 15:08:14 +0500

    using chainlink oracle to fetch usd price

--- Fundme.sol
+++ Fundme.sol
@@ -1,10 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
+import "./PriceConverter.sol";
+
 contract Fundme {
-    uint256 public minimumUsd = 50;
+    using PriceConverter for uint256;
+    uint256 public minimumUsd = 50 * 1e18;
+
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
 
     function fund() public payable {
-        require(msg.value >= minimumUsd, "Didn;t send enough eth");
+        require(
+            msg.value.getConversionRate() >= minimumUsd,
+            "Didn;t send enough eth"
+        );
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
     }
 }
